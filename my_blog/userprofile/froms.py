8856370ca743bcb7f08b80a1465e4dac46e1bf18
zyc <yanchao.zhang-zyc@outlook.com>
2019-07-24 13:58:26 +0800

    增加注册html网页

--- my_blog/userprofile/froms.py
+++ my_blog/userprofile/froms.py
@@ -19,11 +19,9 @@ class UserRegisterForm(forms.ModelForm):
     # 复写 User 的密码
     password = forms.CharField()
     password2 = forms.CharField()
-
     class Meta:
         model = User
         fields = ('username', 'email')
-
     # 对两次输入的密码是否一致进行检查
     def clean_password2(self):
         data = self.cleaned_data
