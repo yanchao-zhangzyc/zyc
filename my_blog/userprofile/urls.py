e6eb5d1c2918365e09bd34b8acf14939949f4b06
zyc <yanchao.zhang-zyc@outlook.com>
2019-07-24 12:37:20 +0800

    增加注册html网页

--- my_blog/userprofile/urls.py
+++ my_blog/userprofile/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #用户登录
     path('login/',views.user_login,name='login'),
     path('logout/',views.user_logout,name='logout'),
+    path('register/',views.user_register,name='register'),
 ]
